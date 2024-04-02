052b42dd88ed89d5f34bd72a5b9a9861ce33fcaf
Caspian Samuelsson <cappeluring@gmail.com>
2024-04-02 15:34:21 +0200

    Player Switching

--- EscapeRoom/Assets/Scripts/PlayerSwitching.cs
+++ EscapeRoom/Assets/Scripts/PlayerSwitching.cs
@@ -1,29 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class PlayerSwitching : MonoBehaviour
 {
-    public Transform character;
-    public List<Transform> characters;
+ 
     public int ActiveChar;
 
+    public Transform FischCam;
+    public Transform BarnyCam;
+    public Transform Maincamera;
+
+    public GameObject Fisch;
+    public GameObject Barny;
+
+    public bool camFollowFisch;
     private void Start()
     {
-       
+        camFollowFisch = true;
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Q))
+        if (camFollowFisch == true)
+        {
+            Maincamera.transform.position = FischCam.transform.position;
+            Fisch.GetComponent<PlayerMovement>().enabled = true;
+            Barny.GetComponent<PlayerMovement>().enabled = false;
+        }
+        else if (camFollowFisch == false)
+        { 
+            Maincamera.position = BarnyCam.transform.position;
+            Fisch.GetComponent<PlayerMovement>().enabled = false;
+            Barny.GetComponent<PlayerMovement>().enabled = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             if(ActiveChar == 0)
             {
-                ActiveChar = characters.Count + 1;
+                ActiveChar += 1;
+                camFollowFisch = true;
             }
             else if(ActiveChar == 1)
             { 
-                ActiveChar = characters.Count - 1;
+                ActiveChar -= 1;
+                camFollowFisch = false;
             }
         }
     }
