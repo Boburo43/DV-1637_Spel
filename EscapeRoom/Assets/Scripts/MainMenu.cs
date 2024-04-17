1f3deb2cc98f26e4e4b60940f0b3f65efd94bbf5
Caspian Samuelsson <cappeluring@gmail.com>
2024-04-17 15:22:27 +0200

    Mainmenu frist edidiotn

---
+++ EscapeRoom/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField] private GameObject optionsMenu;
+    [SerializeField] private GameObject mainMenu;
+
+    private void Start()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+    public void GoToOptions()
+    {
+        optionsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void BackToMain()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
