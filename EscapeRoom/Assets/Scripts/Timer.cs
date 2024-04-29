89f050ff17cef3ce505eecf31d856b570641e984
S7n73r <159285089+S7n73r@users.noreply.github.com>
2024-04-29 11:14:01 +0200

    Timer

--- EscapeRoom/Assets/Scripts/Timer.cs
+++ EscapeRoom/Assets/Scripts/Timer.cs
@@ -2,19 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Playables;
 
 public class Timer : MonoBehaviour
 {
-    private float timeLeft = 600f;
-    public bool timerOn = false;
+    private float timeLeft = 60f;
+    private float totalTime = 60f;
+    public bool timerOn = true;
     [SerializeField] private Cam Camera;
     public static bool lose = false;
+
+    [SerializeField] private GameObject HalfTimeText;
+    float warningActive = 5f;
 
     public TMP_Text TimerTxt;
     // Start is called before the first frame update
     void Start()
     {
         timerOn = true;
+        HalfTimeText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,20 +33,31 @@ public class Timer : MonoBehaviour
                 timeLeft -= Time.deltaTime;
                 updateTimer(timeLeft);
                 lose = false;
+                if(timeLeft <= totalTime / 2 & warningActive > 0)
+                {
+                    //Notifiera spelare
+                    HalfTimeText.gameObject.SetActive(true);
+                    warningActive -= Time.deltaTime;
+                }
+                else
+                {
+                    HalfTimeText.gameObject.SetActive(false);
+                }
             }
             else
             {
+                timerOn = false;
                 lose = true;
             }
+            
         }
     }
     void updateTimer(float currentTime)
     {
         currentTime += 1;
 
-        float minutes = Mathf.FloorToInt(currentTime / 60);
-        float seconds = Mathf.FloorToInt(currentTime % 60);
+        float timeleft = Mathf.FloorToInt((currentTime / totalTime) * 100);
 
-        TimerTxt.text = string.Format("Battery left: {0:00} : {1:00}", minutes, seconds);
+        TimerTxt.text = string.Format("Battery left: {0} %", timeleft);
     }
 }
